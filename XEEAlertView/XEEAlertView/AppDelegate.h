9f51e0968a15a49661af9348c0f7bda89304be68
Andrija Cajic <ancajic@gmail.com>
2013-12-06 15:26:35 +0100

    Initial commit

---
+++ XEEAlertView/XEEAlertView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XEEAlertView
+//
+//  Created by Andrija Cajic on 06/12/13.
+//  Copyright (c) 2013 XEE Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
